80494eaf5782f93d43dc0923d3b6425794a84818
Zakaria <jakariablaine120@gmail.com>
2022-03-30 18:26:59 +0600

    Add migrations for enrollment (these were somehow lost from past commits)

---
+++ database/migrations/2022_03_30_120815_create_enrollment_forms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnrollmentFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enrollment_forms', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('uid')->unique();
+            $table->string('course_code');
+            $table->string('course_title');
+            $table->longText('form_data');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enrollment_forms');
+    }
+}
